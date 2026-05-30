4deefbfe58b6093c3a2ac6d98404dd05c1cb1ab4
shafiul-eng <mdshafiul237@gmail.com>
2026-05-30 11:34:23 +0600

    Delete backend/demoRiddles.js

--- backend/demoRiddles.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  { question: "What has keys but can't open locks?", answer: "Piano", difficulty: 1 },
-  { question: "The more you take, the more you leave behind. What am I?", answer: "Footsteps", difficulty: 2 }
-];
